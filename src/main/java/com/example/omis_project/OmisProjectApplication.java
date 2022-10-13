04facb5ad098501a85485801c6b711b7594af8ab
AminaZhenisova <115710462+AminaZhenisova@users.noreply.github.com>
2022-10-13 19:21:40 +0600

    Initial commit

---
+++ src/main/java/com/example/omis_project/OmisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.omis_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmisProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmisProjectApplication.class, args);
+    }
+
+}
